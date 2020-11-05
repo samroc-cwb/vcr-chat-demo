e5b0887b1605c6e70549d698a133f33b669cf025
samroc <sam@castingworkbook.com>
2020-11-05 13:29:53 -0800

    demo ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Chat, Channel, ChannelList, Window } from 'stream-chat-react';
+import { ChannelHeader, MessageList } from 'stream-chat-react';
+import { MessageInput, Thread } from 'stream-chat-react';
+import { StreamChat } from 'stream-chat';
+import users from './users';
+import 'stream-chat-react/dist/css/index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const chatClient = new StreamChat('gx5a64bj4ptz');
 
-export default App;
+
+const user = users[Math.floor(Math.random() * users.length)];
+const userToken = user.userToken;
+
+console.log(user);
+
+chatClient.setUser(
+    user,
+    userToken,
+);
+
+const filters = { type: 'messaging', members: { $in: ['old-morning-4'] } };
+const sort = { last_message_at: -1 };
+const channels = chatClient.queryChannels(filters, sort);
+
+// const conversation = chatClient.channel('messaging', 'Tenet-ftv', {
+//   name: 'Tenet',
+//   image: 'https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=150&q=80',
+//   members: [users[1].id, users[2].id],
+// });
+
+// conversation.create();
+
+const App = () => (
+  <Chat client={chatClient} theme={'messaging light'}>
+    <ChannelList
+      filters={filters}
+      sort={sort}
+    />
+    <Channel>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
+
+export default App; 
---
+++ src/users.js
@@ -0,0 +1,34 @@
+
+const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoib2xkLW1vcm5pbmctNCJ9.ZwX0bpmkwUVqXWtcP3qizjEAldcjumOQ07cM7WFq-5o';
+
+
+const users = [
+    {
+        id: 'old-morning-4',
+        name: 'Steve Miller',
+        image: 'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=150&q=80',
+        userToken: userToken,
+    },
+    {
+        id: 'jethro-tull',
+        userToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiamV0aHJvLXR1bGwifQ.7uuwepRobpYXbPaaFpQp9ALS9zUjpklZB90xdsLmuZU',
+        name: 'Jethro Tull',
+        image: 'https://images.unsplash.com/photo-1554727242-741c14fa561c?ixlib=rb-1.2.1&auto=format&fit=crop&w=150&q=80'
+    },
+    {
+        id: 'jimi-hendrix',
+        userToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiamltaS1oZW5kcml4In0.3f4bJOu4wvSiNoWV9NV-Xn40Gn1TRacrHfLScU1boa8',
+        name: 'Jimi Hendrix',
+        image: 'https://images.unsplash.com/photo-1525201548942-d8732f6617a0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=150&q=80'
+    },
+    {
+        id: 'kat-holders',
+        userToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoia2F0LWhvbGRlcnMifQ.AcAC6qN1P37c2uOEpvqN98TWfrRIqL8if5O1Bp7ZbQE',
+        name: 'Katrina Hendrix',
+        image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=crop&w=150&q=80'
+    }
+];
+
+
+
+export default users;
